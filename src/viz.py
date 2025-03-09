30e74a5261b59dc173564f9295ea16912951cb22
Neil Holloway <neil_holloway@hotmail.co.uk>
2025-03-09 00:43:42 +0000

    steps

--- src/viz.py
+++ src/viz.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-
+import altair as alt
 from src.data_utils import PERIOD_OPTIONS, drop_withdrawals
 
 
@@ -37,20 +37,37 @@ def earnings_by_period(df: pd.DataFrame, period: str) -> pd.DataFrame:
     temp_df["earnings"] = temp_df["Sub Total"] + temp_df["Tax"]
     resampled = temp_df["earnings"].resample(period, label="left").sum().reset_index()
     resampled.rename(columns={"Date": "date_grouped"}, inplace=True)
-    st.bar_chart(
-        resampled,
-        x="date_grouped",
-        y="earnings",
-        x_label=PERIOD_OPTIONS[period].title(),
-        y_label="Earnings",
+    chart = (
+        alt.Chart(resampled)
+        .mark_bar()
+        .encode(
+            x=alt.X(
+                "date_grouped:T",
+                title=PERIOD_OPTIONS[period].title(),
+            ),
+            y=alt.Y("earnings:Q", title="Earnings"),
+        )
+        .interactive()
     )
+    st.altair_chart(chart, use_container_width=True)
 
 
 def cumulative_balance(df: pd.DataFrame) -> pd.DataFrame:
     df_copy = df.copy()
     df_copy.sort_values(by="Date", inplace=True)
     df_copy["cumulative_balance"] = df_copy["total_amount"].cumsum()
-    st.line_chart(df_copy, x="Date", y="cumulative_balance", y_label="Cumulative balance (£)")
+
+    chart = (
+        alt.Chart(df_copy)
+        .mark_line(interpolate="step-after")
+        .encode(
+            x=alt.X("Date:T", title="Date"),
+            y=alt.Y("cumulative_balance:Q", title="Cumulative balance (£)", axis=alt.Axis(grid=True)),
+        )
+        .interactive()
+    )
+
+    st.altair_chart(chart, use_container_width=True)
 
 
 def cumulative_earnings(df: pd.DataFrame) -> pd.DataFrame:
@@ -58,4 +75,15 @@ def cumulative_earnings(df: pd.DataFrame) -> pd.DataFrame:
     df_copy = drop_withdrawals(df_copy)
     df_copy.sort_values(by="Date", inplace=True)
     df_copy["cumulative_earnings"] = df_copy["total_amount"].cumsum()
-    st.line_chart(df_copy, x="Date", y="cumulative_earnings", y_label="Cumulative earnings (£)")
+
+    chart = (
+        alt.Chart(df_copy)
+        .mark_line(interpolate="step-after")
+        .encode(
+            x=alt.X("Date:T", title="Date"),
+            y=alt.Y("cumulative_earnings:Q", title="Cumulative earnings (£)", axis=alt.Axis(grid=True)),
+        )
+        .interactive()
+    )
+
+    st.altair_chart(chart, use_container_width=True)
